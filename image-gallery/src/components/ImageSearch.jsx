160e10ca2c21c94bec25bda7a5cdcd8bbdfaa3c8
isaacoog1 <oguntuyokoredeshile@gmail.com>
2023-12-29 20:53:00 +0100

    Changes to styles

--- image-gallery/src/components/ImageSearch.jsx
+++ image-gallery/src/components/ImageSearch.jsx
@@ -9,7 +9,7 @@ const ImageSearch = ({searchText}) => {
     }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+    <div className="max-w-xs rounded overflow-hidden my-10 mx-auto sm:max-w-sm">
         <form onSubmit={onsubmit} className="w-full max-w-sm">
             <div className="flex items-center border-b-2 border-teal-500 py-2">
                 <input onChange={event => setText(event.target.value)} type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder='Search Image term...'/>
